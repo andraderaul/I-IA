02938872b5da84790a37aec9167bb2b4d4b1ce0b
Raul Andrade <raul10.3@hotmail.com>
2018-03-12 23:30:20 -0300

    Add files via upload

--- classificador/knn.py
+++ classificador/knn.py
@@ -23,23 +23,17 @@ class Individuo:
 def distEuclidiana(ind1, ind2):
 	soma = pow(ind1.getA() - ind2.getA(),2) + pow(ind1.getB() - ind2.getB(),2) + pow(ind1.getC() - ind2.getC(),2) +	pow(ind1.getD() - ind2.getD(),2)
 	return sqrt(soma)
-	
-def classificarAmostra(individuos, novo_exemplo, K	):
-	tam_vet = len(individuos)
-	dist_individuos = set()
-	temp = dict()
-	for i in range(tam_vet):
-		dist = distEuclidiana(individuos[i], novo_exemplo)
-		temp[dist] = i
-		dist_individuos.union(temp)
-	
+
+def contandoClasse(individuos, vizinhos,K):
 	cont_class = [0 for i in range(3)]	
 	contK = 0
-	for ind in individuos:
+	for i in range(len(individuos)):
+		index = vizinhos[i] #indice do vizinho mais proximo
 		if contK == K:
 			break
-		classe = ind.getClasse()
-		if classe == 'Iris-Setosa':
+		classe = individuos[index].getClasse() #pegando a classe do vizinho mais proximo
+		print('Index: %d, Classe: %s' %(index,classe))
+		if classe == 'Iris-setosa':
 			cont_class[0]+=1 
 		elif classe == 'Iris-versicolor':
 			cont_class[1]+=1
@@ -47,16 +41,30 @@ def classificarAmostra(individuos, novo_exemplo, K	):
 			cont_class[2]+=1
 		contK+=1
 	
-	classe_classificacao = ''
+	return cont_class 	
 	
+def checandoClasse(cont_class):
+	
+	classe_classificacao = ''
 	if cont_class[0] >= cont_class[1] and cont_class[0] >= cont_class[2]:
 		classe_classificacao = 'Iris-setosa'
 	elif cont_class[1] >= cont_class[0] and cont_class[1] >= cont_class[2]:
 		classe_classificacao = 'Iris-versicolor'
 	else:
 		classe_classificacao = 'Iris-virginica'
-	
 	return classe_classificacao			
+	
+def classificarAmostra(individuos, novo_exemplo, K):
+	
+	dist_individuos = dict()
+	for i in range(len(individuos)):
+		dist = distEuclidiana(individuos[i], novo_exemplo)  #calcula a distancia de cada amostra
+		dist_individuos[i] = dist #salva no dicionario
+	
+	k_vizinhos = sorted(dist_individuos,key=dist_individuos.get) #ordena com base na distancia e salva cada indice na lista chamada de k_vizinhos
+	
+	return checandoClasse(contandoClasse(individuos,k_vizinhos,K))
+	
 
 def lerarquivo():
 	amostras = []
@@ -65,20 +73,15 @@ def lerarquivo():
 			atrib = linha.replace('\n', '').split(',')
 			ind = Individuo(float(atrib[0]), float(atrib[1]), float(atrib[2]), float(atrib[3]), (atrib[4]))
 			amostras.append(ind)
-
 	return amostras
         
 def main():
-	K = 3
+	K = 15
 	entrada = lerarquivo()
-
 	ind1 = Individuo(3,3,3,3,'nao_sei')
 	ind2 = Individuo(-1,0,1,2,'nao_sei')
-	
-	print(classificarAmostra(entrada, ind1, K))
-	print(classificarAmostra(entrada, ind2, K))
-	
-	
+	print('Classificacao: %s' %classificarAmostra(entrada, ind1, K))
+	print('Classificacao: %s' %classificarAmostra(entrada, ind2, K))
 	
 main()	
 	
